86c62ea39cb95f22da9d49773d5a532ff5cc4857
Matt Griffin <mattgriffin94@yahoo.com>
2014-04-07 17:04:57 -0700

    Initialize repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bpstats::Application.config.secret_key_base = 'c67023297263bc0b334da924bd8ea5024f559219c0a5714a1b9870b2df38737487f6001ac486f8df0cb329cdd59180e9d9b00151c6cb8d12059ab54bf6c41eb0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bpstats::Application.config.session_store :cookie_store, key: '_bpstats_session'
